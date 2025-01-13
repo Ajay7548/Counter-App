d00127ad0f7deb9d0dafdab441f21ff2c2cbd54d
Ajay7548 <mouryaajay7548>
2025-01-13 17:22:05 +0530

    Counter App

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,53 @@
+
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+ 
+  // Using react
+  const [counter , setCounter] = useState(0)
+
+  //Through javscript
+  // let counter = 1
+  // const addValue = () =>{
+  //   counter = counter + 1 
+  //   const add = document.querySelector('.add')
+  //   add.innerHTML = `Increase Value:- ${counter}`
+  // }
+
+  const addValue = ()=>{
+    if (counter < 10) {
+      setCounter(counter+1)
+    }
+    else{
+      alert('Counter Reached Limit')
+
+    }  
+  }
+
+  const decreaseVal = () => {
+    if(counter > 0){
+      setCounter(counter -1 )
+    }
+    else{
+      alert('Counter Cannot be below Zero')
+    }
+    
+  }
+
+  return (
+    <>
+    <h1>Counter App :- {counter}</h1>
+    {/* addval passd as refeence so it will execute when click */}
+    <button className='add'
+    onClick={addValue}>Increase Value:- {counter}</button>
+    <br />
+    <br />
+    <button
+    onClick={decreaseVal}
+    >Decrease Value:- {counter} </button>
+    </>
+  )
+}
+
+export default App
